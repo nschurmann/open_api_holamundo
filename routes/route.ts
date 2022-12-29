577b139a04044ca7cbe23a2e2ccacd311fc5b90c
Nicolas Schurmann <nicolas@Nicolass-MacBook-Pro.local>
2022-12-29 11:14:27 -0500

    change formatting

--- routes/route.ts
+++ routes/route.ts
@@ -1,29 +1,29 @@
 import { Router } from "express"
 
 function route(resources: any[]) {
-    const router = Router()
+  const router = Router()
 
-    router.get("/", (_req, res) => {
-        res.status(200).json(resources)
-    })
+  router.get("/", (_req, res) => {
+    res.status(200).json(resources)
+  })
 
-    router.post("/", (_req, res) => {
-        res.status(201).send({ id: resources.length + 1 })
-    })
+  router.post("/", (_req, res) => {
+    res.status(201).send({ id: resources.length + 1 })
+  })
 
-    router.put("/", (_req, res) => {
-        res.sendStatus(204)
-    })
+  router.put("/", (_req, res) => {
+    res.sendStatus(204)
+  })
 
-    router.patch("/", (_req, res) => {
-        res.sendStatus(204)
-    })
+  router.patch("/", (_req, res) => {
+    res.sendStatus(204)
+  })
 
-    router.delete("/", (_req, res) => {
-        res.sendStatus(204)
-    })
+  router.delete("/", (_req, res) => {
+    res.sendStatus(204)
+  })
 
-    return router
+  return router
 }
 
 export default route
